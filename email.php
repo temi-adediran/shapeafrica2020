c14b44fbddd1828992e1d5c31053f261e10a3e73
andela-tfowotade <temitope.fowotade@andela.com>
2020-02-05 17:33:41 +0100

    modifies contact form

--- email.php
+++ email.php
@@ -1,37 +1,51 @@
 <?php
-$subject = 'You Got Message'; // Subject of your email
-$to = 'fowotop@gmail.com';  //Recipient's E-mail
-$emailTo = $_REQUEST['email'];
 
-$name = $_REQUEST['name'];
-$email = $_REQUEST['email'];
-$phone = $_REQUEST['phone'];
-$ticket_type = $_REQUEST['ticket-type'];
-$msg = $_REQUEST['message'];
+// Only process POST reqeusts.
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the form fields and remove whitespace.
+    $name = strip_tags(trim($_POST["name"]));
+    $name = str_replace(array("\r","\n"),array(" "," "),$name);
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = trim($_POST["message"]);
 
-$email_from = $name.'<'.$email.'>';
+    // Check that data was sent to the mailer.
+    if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // Set a 400 (bad request) response code and exit.
+        http_response_code(400);
+        echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+        exit;
+    }
 
-$headers = "MIME-Version: 1.1";
-$headers .= "Content-type: text/html; charset=iso-8859-1";
-$headers .= "From: ".$name.'<'.$email.'>'."\r\n"; // Sender's E-mail
-$headers .= "Return-Path:"."From:" . $email;
+    // Set the recipient email address.
+    // FIXME: Update this to your desired email address.
+    $recipient = "fowotop@gmail.com";
 
-$message .= 'Name : ' . $name . "\n";
-$message .= 'Email : ' . $email . "\n";
-$message .= 'Phone : ' . $phone . "\n";
-$message .= 'Ticket : ' . $ticket_type . "\n";
-$message .= 'Message : ' . $msg;
+    // Set the email subject.
+    $subject = "New message from $name";
 
-if (@mail($to, $subject, $message, $email_from))
-{
-  http_response_code(200);
-	// Transfer the value 'sent' to ajax function for showing success message.
-	echo 'sent';
+    // Build the email content.
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Build the email headers.
+    $email_headers = "From: $name <$email>";
+
+    // Send the email.
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        // Set a 200 (okay) response code.
+        http_response_code(200);
+        echo "Thank You! Your message has been sent.";
+    } else {
+        // Set a 500 (internal server error) response code.
+        http_response_code(500);
+        echo "Oops! Something went wrong and we couldn't send your message.";
+    }
+
+} else {
+    // Not a POST request, set a 403 (forbidden) response code.
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
 }
-else
-{
-  http_response_code(500);
-	// Transfer the value 'failed' to ajax function for showing error message.
-	echo 'failed';
-}
+
 ?>
